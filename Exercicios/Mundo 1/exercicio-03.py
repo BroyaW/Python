86866e57ad6781a425780042f101d371aa47f996
BroyaW <brayan.s.borba147@gmail.com>
2023-06-07 18:29:16 -0300

    exercicio 3 concluido

--- Exercicios/Mundo 1/exercicio-03.py
+++ Exercicios/Mundo 1/exercicio-03.py
@@ -1,3 +1,4 @@
-n1 = input ('primeiro numero hhaha')
-n2 = input ('segundo numero hahahha')
-print (n1 + n2)
+n1 = int (input ('primeiro numero '))
+n2 = int (input ('segundo numero '))
+n3 = n1 + n2
+print('a soma do',n1,'e do',n2,'da',n3,)
