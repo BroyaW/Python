2d6b53b255e443bee23a7dc6dbd5f6f1feb501b8
BroyaW <brayan.s.borba147@gmail.com>
2023-06-14 18:47:03 -0300

    sonic boom x episodio 999M

---
+++ Exercicios/Mundo 1/exercicio-6-verdadeiro.py
@@ -0,0 +1,5 @@
+n1 = int (input ('bote um numero '))
+n2 = n1*2
+n3 = n1*3
+n4 = n1** (1/2)
+print ('o numero',n1,'multiplicando por 2 da',n2,'e multiplicando por 3 da',n3,'e a raiz quadrada de',n1,'é',n4,)
---
+++ Exercicios/Mundo 1/exercicio-7-a-1.py
@@ -0,0 +1,6 @@
+n1 = float (input ('que nota jazz ghost tirou? '))
+n2 = float (input ('que nota nicolas borba tirou? '))
+n3 = float (input ('que nota felipe neto tirou? '))
+n4 = n1 + n2 + n3
+n5 = n4/3
+print ('juntando todas as notas da',n4,'e dividindo ela da',n5,)
