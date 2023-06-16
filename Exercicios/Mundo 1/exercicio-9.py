db67f14557e26293c9375446c881fd9a8c381d24
BroyaW <brayan.s.borba147@gmail.com>
2023-06-16 19:16:39 -0300

    chato

---
+++ Exercicios/Mundo 1/exercicio-9.py
@@ -0,0 +1,14 @@
+n = int (input ('bote um numero '))
+n0 = n*1
+n2 = n*2
+n3 = n*3
+n4 = n*4
+n5 = n*5
+n6 = n*6
+n7 = n*7
+n8 = n*8
+n9 = n*9
+n10 = n*10
+n20 = n0,n2,n3,n4,n5,n6,n7,n8,n9,n10
+print ('a tabuada do numero',n,'é')
+print ('tabuada',n20,)
