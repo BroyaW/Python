b30ef37457404118ce19e11fd83191367cc977c3
BroyaW <brayan.s.borba147@gmail.com>
2023-06-17 11:54:16 -0300

    mm

---
+++ Exercicios/Mundo 1/exercicio-10.py
@@ -0,0 +1,4 @@
+money = float (input ('bote um numero pra ser money '))
+dollar = money/4.82
+print (money,'reais em dollar é',dollar)
+print("{} reais em dollar é {:.999999999999999f}".format(money,dollar))
