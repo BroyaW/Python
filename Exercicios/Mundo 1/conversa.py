b2d476e1f0abc16f844f2df3f3d44e1b65403ee2
BroyaW <brayan.s.borba147@gmail.com>
2023-06-18 15:07:58 -0300

    jogo

--- Exercicios/Mundo 1/conversa.py
+++ Exercicios/Mundo 1/conversa.py
@@ -1,6 +1,59 @@
-a=str (input('oi tudo bem? ')) 
-a=str (input('quer ser meu amigo? '))
-a=str (input('oque vc esta fazendo? '))
-a=str (input('vou te contar uma piada '))
-a=str (input('por que o menino estava falando ao telefone deitado? '))
-print ('para nao cair a ligaçao')
+def exibir_tabuleiro(tabuleiro):
+    for linha in tabuleiro:
+        print("|".join(linha))
+        print("-----")
+
+
+def verificar_vitoria(tabuleiro, jogador):
+    # Verificar linhas
+    for linha in tabuleiro:
+        if linha.count(jogador) == 3:
+            return True
+
+    # Verificar colunas
+    for coluna in range(3):
+        if tabuleiro[0][coluna] == tabuleiro[1][coluna] == tabuleiro[2][coluna] == jogador:
+            return True
+
+    # Verificar diagonais
+    if tabuleiro[0][0] == tabuleiro[1][1] == tabuleiro[2][2] == jogador:
+        return True
+    if tabuleiro[0][2] == tabuleiro[1][1] == tabuleiro[2][0] == jogador:
+        return True
+
+    return False
+
+
+def jogar_jogo_da_velha():
+    tabuleiro = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
+    jogador_atual = "X"
+
+    while True:
+        exibir_tabuleiro(tabuleiro)
+        linha = int(input("Digite o número da linha (0, 1 ou 2): "))
+        coluna = int(input("Digite o número da coluna (0, 1 ou 2): "))
+
+        if tabuleiro[linha][coluna] != " ":
+            print("Essa posição já está ocupada. Tente novamente.")
+            continue
+
+        tabuleiro[linha][coluna] = jogador_atual
+
+        if verificar_vitoria(tabuleiro, jogador_atual):
+            exibir_tabuleiro(tabuleiro)
+            print("Parabéns! O jogador", jogador_atual, "venceu!")
+            break
+
+        if jogador_atual == "X":
+            jogador_atual = "O"
+        else:
+            jogador_atual = "X"
+
+        # Verificar se deu empate
+        if all(tabuleiro[i][j] != " " for i in range(3) for j in range(3)):
+            exibir_tabuleiro(tabuleiro)
+            print("Empate!")
+            break
+
+
+jogar_jogo_da_velha()
---
+++ Exercicios/Mundo 1/desafio-mais-dificil-que-os-outros.py
@@ -0,0 +1,7 @@
+n1 = int(input('Quer ser meu amigo? (1 = sim | 0 = não): '))
+
+if n1 == 1:
+    print("Eba")
+    n2 = int(input('Digite um número: '))
+else:
+    print("Eu te odeio, chato idiota doente. Eu vou te matar.")
