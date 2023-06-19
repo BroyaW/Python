8880524610b527906cfa5b1250da6352076a4dec
BroyaW <brayan.s.borba147@gmail.com>
2023-06-19 20:17:45 -0300

    ss

---
+++ Exercicios/Mundo 1/exercicio-11.py
@@ -0,0 +1,5 @@
+n1 = float (input ('bote um  numero pra ser altura '))
+n2 = float (input ('bote um  numero pra ser largura '))
+n3 = n1*n2
+n4 = n3/2
+print ('essa parede tem',n1,'de altura e tem',n2,'de largura e precisa de ',n4,'litros de tinta')
